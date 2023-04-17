482c27a3721c48cedb11b610dee81038ebe209bd
“eggmun98” <eggmun98@gmail.com>
2023-04-17 18:07:54 +0900

    [2023.04.17] Design header 헤더 스타일 적용

--- src/components/commons/layout/header/index.tsx
+++ src/components/commons/layout/header/index.tsx
@@ -1,3 +1,27 @@
+import {
+  LoginWrapper,
+  Logo,
+  LogoWrapper,
+  MenuWrapper,
+  Wrapper,
+} from "./styles";
+
 export default function LayoutHeader(): JSX.Element {
-  return <></>;
+  return (
+    <Wrapper>
+      <div>
+        <LogoWrapper>
+          <Logo src="/layout/header/mpLogo.png"></Logo>
+        </LogoWrapper>
+        <MenuWrapper>
+          <h1>커뮤니티</h1>
+          <h1>등산 마켓</h1>
+          <h1>오늘의 룩</h1>
+        </MenuWrapper>
+        <LoginWrapper>
+          <button>login</button>
+        </LoginWrapper>
+      </div>
+    </Wrapper>
+  );
 }
