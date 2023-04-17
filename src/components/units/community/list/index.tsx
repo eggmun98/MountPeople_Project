d61c0c5bb180d398dcc2547205f9c630b010b71d
“eggmun98” <eggmun98@gmail.com>
2023-04-17 19:21:26 +0900

    [2023.04.17] Design communityPage 스타일 적용

--- src/components/units/community/list/index.tsx
+++ src/components/units/community/list/index.tsx
@@ -1,13 +1,14 @@
 import ListBody from "./body";
 import ListFooter from "./footer";
 import ListHeader from "./header";
+import { Wrapper } from "./styles";
 
 export default function CommunityListUI(): JSX.Element {
   return (
-    <>
+    <Wrapper>
       <ListHeader></ListHeader>
       <ListBody></ListBody>
       <ListFooter></ListFooter>
-    </>
+    </Wrapper>
   );
 }
