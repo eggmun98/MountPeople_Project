a06967688fe97011334afc91d05e41b4112352e6
“eggmun98” <eggmun98@gmail.com>
2023-04-17 15:36:23 +0900

    [2023.04.17] Setting 레이아웃구조 세팅, 글로벌스타일 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
-// import "../styles/globals.css";
+import { Global } from "@emotion/react";
+import { AppProps } from "next/app";
 import ApolloSetting from "../src/commons/apollo";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import Layout from "../src/components/commons/layout";
 
-export default function App({ Component }) {
+export default function App({ Component }: AppProps): JSX.Element {
   return (
     <ApolloSetting>
-      <Component />
+      <>
+        <Global styles={globalStyles} />
+        <Layout>
+          <Component />
+        </Layout>
+      </>
     </ApolloSetting>
   );
 }
---
+++ src/commons/styles/globalStyles.ts
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    font-family: "Regular";
+  }
+`;
