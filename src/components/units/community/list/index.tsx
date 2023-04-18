06351eaba0ceaaeaf3c9012e579e9be7d75d152d
“eggmun98” <eggmun98@gmail.com>
2023-04-18 14:04:51 +0900

    [2023.04.18] Feat pagination 기능 구현

--- src/components/units/community/list/index.tsx
+++ src/components/units/community/list/index.tsx
@@ -1,14 +1,16 @@
+import { useQueryFetchBoards } from "../../../commons/hooks/query/useQueryFetchBoards";
 import ListBody from "./body";
 import ListFooter from "./footer";
 import ListHeader from "./header";
 import { Wrapper } from "./styles";
 
 export default function CommunityListUI(): JSX.Element {
+  const { data, refetch } = useQueryFetchBoards();
   return (
     <Wrapper>
       <ListHeader></ListHeader>
-      <ListBody></ListBody>
-      <ListFooter></ListFooter>
+      <ListBody data={data}></ListBody>
+      <ListFooter data={data} refetch={refetch}></ListFooter>
     </Wrapper>
   );
 }
