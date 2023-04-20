865359938d6ae4beb82aaf6f44dd68673dea7cd1
“eggmun98” <eggmun98@gmail.com>
2023-04-20 23:22:50 +0900

    [2023.04.20] Feat 게시글 작성 버튼

---
+++ src/components/commons/hooks/customs/useCrateBoardMode.ts
@@ -0,0 +1,33 @@
+import { IData } from "../../../units/community/write";
+import { useMutationCrateBoard } from "../mutation/useMutationCrateBoard";
+
+export const useCreateBoardMode = (): {
+  onClickCreateBoard: (data: IData) => Promise<void>;
+} => {
+  const [createBoard] = useMutationCrateBoard();
+
+  const onClickCreateBoard = async (data: IData): Promise<void> => {
+    try {
+      await createBoard({
+        variables: {
+          createBoardInput: {
+            writer: data.writer,
+            title: String(data.title),
+            password: data.password,
+            contents: String(data.contents),
+            youtubeUrl: data.youtubeUrl,
+            boardAddress: {
+              addressDetail: data.addressDetail,
+            },
+          },
+        },
+      });
+      alert("게시글 작성하였습니다.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return {
+    onClickCreateBoard,
+  };
+};
