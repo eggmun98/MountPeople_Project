de38d980d7c4acfa34440d4d8c92d09f55d13a7c
“eggmun98” <eggmun98@gmail.com>
2023-04-21 14:36:31 +0900

    [2023.04.21] Design 커뮤니티 디테일페이지 스타일 적용

--- src/components/units/community/detail/index.tsx
+++ src/components/units/community/detail/index.tsx
@@ -1,3 +1,10 @@
-export default function CommunityDetailUI(): JSX.Element {
-  return <></>;
+import DetailHeader from "./header";
+import { Wrapper } from "./styles";
+
+export default function CommunityDetail(): JSX.Element {
+  return (
+    <Wrapper>
+      <DetailHeader></DetailHeader>
+    </Wrapper>
+  );
 }
