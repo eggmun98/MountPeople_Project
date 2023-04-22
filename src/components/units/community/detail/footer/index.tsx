9e64cf41f1eaa05b65402c11e293e4a07a96c928
“eggmun98” <eggmun98@gmail.com>
2023-04-22 19:33:22 +0900

    [2023.04.22] Feat 커뮤니티 디테일 페이지 삭제 기능, 수정 페이지 이동, 목록 이동 기능

---
+++ src/components/units/community/detail/footer/index.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import { movePageMode } from "../../../../commons/hooks/customs/movePageMode";
+import * as D from "./styles";
+import { useDeleteBoardMode } from "../../../../commons/hooks/customs/community/useDeleteBoardMode";
+
+export default function DetailFooter(): JSX.Element {
+  const { onClickMovePage } = movePageMode();
+  const { onClickDeleteButton } = useDeleteBoardMode();
+  const router = useRouter();
+
+  return (
+    <D.Wrapper>
+      <button
+        onClick={onClickMovePage(
+          `/communitys/community/${String(router.query.page)}/edit`
+        )}
+      >
+        수정 하기
+      </button>
+      <button onClick={onClickMovePage("/communitys")}>목록 가기</button>
+      <button onClick={onClickDeleteButton}>삭제 하기</button>
+    </D.Wrapper>
+  );
+}
--- src/components/units/community/detail/index.tsx
+++ src/components/units/community/detail/index.tsx
@@ -1,4 +1,5 @@
 import CommunityComment from "../comment";
+import DetailFooter from "./footer";
 import DetailHeader from "./header";
 import { Wrapper } from "./styles";
 
@@ -6,6 +7,7 @@ export default function CommunityDetail(): JSX.Element {
   return (
     <Wrapper>
       <DetailHeader></DetailHeader>
+      <DetailFooter></DetailFooter>
       <CommunityComment></CommunityComment>
     </Wrapper>
   );
