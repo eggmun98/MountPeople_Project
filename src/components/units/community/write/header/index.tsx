083d72295773542abd7ae6149fbb6ab2760abc06
“eggmun98” <eggmun98@gmail.com>
2023-04-22 19:01:06 +0900

    [2023.04.22] Feat 게시글 수정기능, 게시글 디폴트밸류

--- src/components/units/community/write/header/index.tsx
+++ src/components/units/community/write/header/index.tsx
@@ -5,20 +5,32 @@ export default function WriteHeader(props: IProps): JSX.Element {
   return (
     <W.Wrapper>
       <W.TextWrapper>
-        <h1>게시글 등록</h1>
+        <h1>{props.isEdit ? "게시글 수정" : "게시글 등록"}</h1>
       </W.TextWrapper>
       <W.WriteWrapper>
-        <input {...props.register("writer")}></input>
+        <input
+          {...props.register("writer")}
+          defaultValue={String(props.data?.fetchBoard.writer)}
+        ></input>
         <input {...props.register("password")}></input>
       </W.WriteWrapper>
       <W.InputWrapper01>
-        <input {...props.register("title")}></input>
+        <input
+          {...props.register("title")}
+          defaultValue={props.data?.fetchBoard.title}
+        ></input>
       </W.InputWrapper01>
       <W.ContentsWrapper>
-        <textarea {...props.register("contents")}></textarea>
+        <textarea
+          {...props.register("contents")}
+          defaultValue={props.data?.fetchBoard?.contents}
+        ></textarea>
       </W.ContentsWrapper>
       <W.InputWrapper01>
-        <input {...props.register("youtubeUrl")}></input>
+        <input
+          {...props.register("youtubeUrl")}
+          defaultValue={props.data?.fetchBoard.youtubeUrl ?? ""}
+        ></input>
       </W.InputWrapper01>
     </W.Wrapper>
   );
