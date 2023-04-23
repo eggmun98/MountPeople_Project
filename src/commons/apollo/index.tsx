d929608db8ead0c0164a53c737949128e8ddc6c0
“eggmun98” <eggmun98@gmail.com>
2023-04-23 19:35:19 +0900

    [2023.04.23] Feat 로그인, 회원가입 기능

--- src/commons/apollo/index.tsx
+++ src/commons/apollo/index.tsx
@@ -3,8 +3,17 @@ import {
   ApolloLink,
   ApolloProvider,
   InMemoryCache,
+  fromPromise,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import {
+  accessTokenState,
+  restoreAccessTokenLoadable,
+} from "../../components/commons/stores";
+import { useRecoilState, useRecoilValueLoadable } from "recoil";
+import { onError } from "@apollo/client/link/error";
+import { getAccessToken } from "../libraries/getAccessToken";
+import { useEffect } from "react";
 
 const GLOBAL_STATE = new InMemoryCache();
 
@@ -13,12 +22,43 @@ interface IApolloSettingProps {
 }
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+  const accessTokenFn = useRecoilValueLoadable(restoreAccessTokenLoadable);
+
+  useEffect(() => {
+    void accessTokenFn.toPromise().then((newAccessToken) => {
+      setAccessToken(newAccessToken ?? "");
+    });
+  }, []);
+
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (typeof graphQLErrors !== "undefined") {
+      for (const err of graphQLErrors) {
+        if (err.extensions.code === "UNAUTHENTICATED") {
+          return fromPromise(
+            getAccessToken().then((newAccessToken) => {
+              setAccessToken(newAccessToken ?? "");
+              operation.setContext({
+                headers: {
+                  ...operation.getContext().headers,
+                  Authorization: `Bearer ${String(newAccessToken)}`,
+                },
+              });
+            })
+          ).flatMap(() => forward(operation));
+        }
+      }
+    }
+  });
+
   const uploadLink = createUploadLink({
     uri: "https://backend-practice.codebootcamp.co.kr/graphql",
+    headers: { Authorization: `Bearer ${accessToken}` },
+    credentials: "include",
   });
 
   const client = new ApolloClient({
-    link: ApolloLink.from([uploadLink]),
+    link: ApolloLink.from([errorLink, uploadLink]),
     cache: GLOBAL_STATE,
   });
 
