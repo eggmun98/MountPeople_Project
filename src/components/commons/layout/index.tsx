9bb98af0780a7377a68191769ee6478e88361146
“eggmun98” <eggmun98@gmail.com>
2023-04-23 18:22:21 +0900

    [2023.04.23] Feat 레이아웃 헤더 특정 페이지 숨기기

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,13 +1,20 @@
+import { useRouter } from "next/router";
 import LayoutHeader from "./header";
+import { includes } from "lodash";
 
 interface ILayoutProps {
   children: JSX.Element;
 }
 
+const HIDDEN_PAGE = ["/sign/signIn", "/sign/signUp"];
+
 export default function Layout(props: ILayoutProps): JSX.Element {
+  const router = useRouter();
+  const hiddenPage = HIDDEN_PAGE.includes(router.asPath);
+
   return (
     <>
-      <LayoutHeader></LayoutHeader>
+      {!hiddenPage && <LayoutHeader></LayoutHeader>}
       <div>{props.children}</div>
     </>
   );
