f02f773393c36bf014a5f0dee4c0fd8c70c0e552
“eggmun98” <eggmun98@gmail.com>
2023-04-24 12:43:35 +0900

    [2023.04.24] Design 마켓 리스트 페이지 스타일 적용

---
+++ src/components/units/market/list/footer/index.tsx
@@ -0,0 +1,26 @@
+import * as L from "./styles";
+
+export default function ListFooter(): JSX.Element {
+  return (
+    <L.Wrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+      <L.ProductWrapper></L.ProductWrapper>
+    </L.Wrapper>
+  );
+}
