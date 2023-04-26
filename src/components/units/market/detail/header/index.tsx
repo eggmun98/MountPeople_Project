895ef129ff8eaa34675715492131cc669d235d82
eggmun98 <eggmun98@gmail.com>
2023-04-26 16:05:53 +0900

    [2023.04.26] Feat 디테일 페이지 이동 기능

--- src/components/units/market/detail/header/index.tsx
+++ src/components/units/market/detail/header/index.tsx
@@ -2,6 +2,7 @@ import * as D from "./styles";
 import { IProps } from "./types";
 
 export default function DetailHeader(props: IProps): JSX.Element {
+  console.log("마켓 디테일 데이터", props.data?.fetchUseditem?.tags);
   return (
     <D.Wrapper>
       <D.MainImgWrapper>
@@ -14,20 +15,18 @@ export default function DetailHeader(props: IProps): JSX.Element {
         </D.TitleWrapper>
         <D.PriceWrapper>
           <p>{props.data?.fetchUseditem.price}</p>
-          <p>
-            {props.data?.fetchUseditem.createdAt
-              .slice(0, 10)
-              .replaceAll("-", ".")}
-          </p>
+          <p>{props.data?.fetchUseditem.createdAt}</p>
         </D.PriceWrapper>
         <D.RemarksWrapper>
           <p>{props.data?.fetchUseditem.remarks}</p>
           <D.Heart></D.Heart>
         </D.RemarksWrapper>
         <D.TagWrapper>
-          {props.data?.fetchUseditem?.tags.map((el) => (
-            <div key={el}>{el}</div>
-          ))}
+          {props.data?.fetchUseditem?.tags
+            ?.filter((el) => el !== "")
+            .map((el) => (
+              <div key={el}>{el}</div>
+            ))}
         </D.TagWrapper>
       </D.ContentsWrapper>
     </D.Wrapper>
