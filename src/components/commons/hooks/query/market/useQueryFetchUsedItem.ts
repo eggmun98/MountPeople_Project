b7cca3dd119c9a2ad218b5228f5f99ccd33592f0
“eggmun98” <eggmun98@gmail.com>
2023-04-26 15:48:17 +0900

    [2023.04.26] Feat 마켓 디테일 페이지 데이터 적용

---
+++ src/components/commons/hooks/query/market/useQueryFetchUsedItem.ts
@@ -0,0 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+import {
+  IQuery,
+  IQueryFetchBoardArgs,
+} from "../../../../../commons/types/generated/types";
+
+export const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      _id
+      name
+      remarks
+      contents
+      price
+      images
+      tags
+      seller {
+        name
+        _id
+      }
+      useditemAddress {
+        address
+      }
+    }
+  }
+`;
+
+export const useQueryFetchUsedItem = (): typeof result => {
+  const router = useRouter();
+  const result = useQuery<Pick<IQuery, "fetchUseditem">, IQueryFetchBoardArgs>(
+    FETCH_USEDITEM,
+    {
+      variables: {
+        useditemId: router.query.page,
+      },
+    }
+  );
+  return result;
+};
