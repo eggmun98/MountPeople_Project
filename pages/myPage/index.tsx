5c682c620d2c5f63e0195208091b59a806be1ac7
eggmun98 <eggmun98@gmail.com>
2023-04-28 17:24:29 +0900

    [2023.04.28] Design 마이페이지 스타일 적용

---
+++ pages/myPage/index.tsx
@@ -0,0 +1,5 @@
+import MyPageUI from "../../src/components/units/myPage";
+
+export default function MyPage(): JSX.Element {
+  return <MyPageUI></MyPageUI>;
+}
---
+++ src/components/units/myPage/footer/index.tsx
@@ -0,0 +1,3 @@
+export default function MyPageFooter(): JSX.Element {
+  return <></>;
+}
