afb94e6daa1004c809280b0b75a8c983a2bbc16a
eggmun98 <eggmun98@gmail.com>
2023-04-29 16:44:04 +0900

    [2023.04.29] Feat 상품 페이지 대댓글 수정, 삭제, 등록, 목록 구현

--- src/components/commons/stores/index.tsx
+++ src/components/commons/stores/index.tsx
@@ -13,6 +13,17 @@ export const originIndexState02 = atom<number>({
   default: -1,
 });
 
+// 마켓 댓글 대댓글 열기 닫기
+export const answerIndexState = atom<number>({
+  key: "answerIndexState",
+  default: -1,
+});
+
+export const answerEditIndexState = atom<number>({
+  key: "answerEditIndexState",
+  default: -1,
+});
+
 // 커뮤니티 우편번호
 export const zipCodeState = atom<string>({
   key: "zipCodeState",
--- src/components/units/market/comment/list/answer/list/edit/index.tsx
+++ src/components/units/market/comment/list/answer/list/edit/index.tsx
@@ -1,3 +1,30 @@
-export default function AnswerEdit(): JSX.Element {
-  return <></>;
+import { useForm } from "react-hook-form";
+import * as A from "../styles";
+import { useUpdateAnswerMode } from "../../../../../../../commons/hooks/customs/market/useUpdateAnswerMode";
+
+export default function AnswerEdit(props): JSX.Element {
+  const { handleSubmit, register } = useForm();
+  const { onClickUpdateAnswer } = useUpdateAnswerMode();
+
+  return (
+    <A.Wrapper>
+      <A.LeftWrapper>
+        <div></div>
+      </A.LeftWrapper>
+      <A.AnswerWrapper>
+        <form
+          onSubmit={handleSubmit(onClickUpdateAnswer(props.aId, props.qId))}
+        >
+          <A.TopWrapper>
+            <div></div>
+            <A.ButtonWrapper>
+              <button>수정하기</button>
+              <button type="button">취소하기</button>
+            </A.ButtonWrapper>
+          </A.TopWrapper>
+          <A.Input01 {...register("contents")}></A.Input01>
+        </form>
+      </A.AnswerWrapper>
+    </A.Wrapper>
+  );
 }
