0c4360c261f4abc7be245107b91e3fa6fe9fa7e8
eggmun98 <eggmun98@gmail.com>
2023-04-30 14:59:18 +0900

    [2023.04.30] Feat 페이지 이동 기능

---
+++ src/components/commons/hooks/customs/allPageLink.ts
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+
+export const useAllPageLink = (): {
+  ALL_PAGE: {
+    community: string[];
+    market: string[];
+  };
+} => {
+  const router = useRouter();
+
+  const ALL_PAGE = {
+    community: [
+      `/communitys/community/${String(router.query.page)}`,
+      `/communitys/community/${String(router.query.page)}/edit`,
+      "/communitys",
+    ],
+    market: [
+      `/markets/market/${String(router.query.page)}`,
+      `/markets/market/${String(router.query.page)}/edit`,
+      "/markets",
+    ],
+  };
+
+  return {
+    ALL_PAGE,
+  };
+};
