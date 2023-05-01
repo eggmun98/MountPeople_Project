57b9926324330e690cb4e414d0b7962403d314a6
eggmun98 <eggmun98@gmail.com>
2023-05-01 18:52:06 +0900

    [2023.05.01] Feat 상품 등록 페이지 이동 기능

--- src/components/units/market/list/body/index.tsx
+++ src/components/units/market/list/body/index.tsx
@@ -1,10 +1,14 @@
+import { movePageMode } from "../../../../commons/hooks/customs/movePageMode";
 import * as L from "./styles";
 
 export default function ListBody(): JSX.Element {
+  const { onClickMovePage } = movePageMode();
+
   return (
     <L.Wrapper>
       <L.TitleWrapper>
         <h1>오늘의 인기 등산물품</h1>
+        <button onClick={onClickMovePage("/markets/new")}>상품 등록</button>
       </L.TitleWrapper>
       <L.ListWrapper>
         <L.ProductWrapper></L.ProductWrapper>
