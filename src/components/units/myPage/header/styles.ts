6cc4387b86363584052a318d41077a2ff290df3d
eggmun98 <eggmun98@gmail.com>
2023-05-02 18:19:43 +0900

    [2023.05.02] Docs 마이페이지 스타일 변경

--- src/components/units/myPage/header/styles.ts
+++ src/components/units/myPage/header/styles.ts
@@ -3,20 +3,20 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
-  padding: 40px 300px;
+  justify-content: flex-start;
+  padding: 40px 0px;
   border-bottom: 1px solid #c5c5c5;
   > h1 {
     font-size: 20px;
     font-weight: 400;
-
+    margin-right: 20px;
     color: 292929;
     :hover {
       color: #3a6625;
     }
   }
   @media (max-width: 1023px) {
-    padding: 20px 150px;
+    padding: 20px 0px;
     > h1 {
       font-size: 18px;
     }
@@ -24,7 +24,7 @@ export const Wrapper = styled.div`
 
   @media (max-width: 767px) {
     margin-top: 20px;
-    padding: 20px 0px;
+    padding: 20px 10px;
     border-top: 1px solid #c5c5c5;
     border-bottom: none;
     > h1 {
