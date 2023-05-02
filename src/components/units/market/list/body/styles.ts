94332a8d40668ae15854d8976900731f38d66697
eggmun98 <eggmun98@gmail.com>
2023-05-02 12:58:54 +0900

    [2023.05.02] Feat+Docs 베스트 상품 목록, 검색바 스타일 변경

--- src/components/units/market/list/body/styles.ts
+++ src/components/units/market/list/body/styles.ts
@@ -56,20 +56,58 @@ export const ListWrapper = styled.div`
 `;
 
 export const ProductWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
   width: 24%;
   height: 300px;
-  border-radius: 5px;
-  background-color: #d9d9d9;
+  margin: 20px 0.1px;
 
   @media (max-width: 1023px) {
     width: 32%;
     height: 250px;
-    margin: 20px 0.1px;
   }
 
   @media (max-width: 767px) {
     width: 49%;
     height: 200px;
-    margin: 20px 0.1px;
+  }
+`;
+export const ProductImg = styled.img`
+  width: 100%;
+  height: 270px;
+  object-fit: cover;
+  border-radius: 5px;
+  @media (max-width: 1023px) {
+    height: 225px;
+  }
+
+  @media (max-width: 767px) {
+    height: 180px;
+  }
+`;
+
+export const HiddenImg = styled.div`
+  width: 100%;
+  height: 270px;
+  background-color: #d9d9d9d9;
+  border-radius: 5px;
+  @media (max-width: 1023px) {
+    height: 225px;
+  }
+
+  @media (max-width: 767px) {
+    height: 180px;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  height: 30px;
+  padding: 10px 20px;
+  > p {
+    font-size: 16px;
+  }
+  > p:nth-of-type(2) {
+    font-size: 14px;
   }
 `;
