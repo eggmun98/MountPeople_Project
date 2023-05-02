84472dc4d527cc67c208948e07992ed1cb7a68ca
eggmun98 <eggmun98@gmail.com>
2023-05-02 15:33:09 +0900

    [2023.05.02] 상품 상세페이지 삭제 기능

---
+++ src/components/units/market/detail/footer/index.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { movePageMode } from "../../../../commons/hooks/customs/movePageMode";
+import * as D from "./styles";
+import { useDeleteBoardMode } from "../../../../commons/hooks/customs/community/useDeleteBoardMode";
+import { useDeleteProductMode } from "../../../../commons/hooks/customs/market/useDeleteProductMode";
+
+export default function DetailFooter(): JSX.Element {
+  const { onClickMovePage } = movePageMode();
+  const { onClickDeleteProduct } = useDeleteProductMode();
+  const router = useRouter();
+
+  return (
+    <D.Wrapper>
+      <button
+        onClick={onClickMovePage(
+          `/markets/market/${String(router.query.page)}/edit`
+        )}
+      >
+        수정 하기
+      </button>
+      <button onClick={onClickMovePage("/markets")}>목록 가기</button>
+      <button onClick={onClickDeleteProduct}>삭제 하기</button>
+    </D.Wrapper>
+  );
+}
