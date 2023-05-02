d94d74f9df2108141a7ff647e4777f613379439c
eggmun98 <eggmun98@gmail.com>
2023-05-02 14:48:06 +0900

    [2023.05.02] Docs 파일명 수정과 타입 수정

--- src/components/commons/hooks/query/market/useQueryFetchUsedItem.ts
+++ src/components/commons/hooks/query/market/useQueryFetchUsedItem.ts
@@ -2,7 +2,7 @@ import { gql, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import {
   IQuery,
-  IQueryFetchBoardArgs,
+  IQueryFetchUseditemArgs,
 } from "../../../../../commons/types/generated/types";
 
 export const FETCH_USED_ITEM = gql`
@@ -28,13 +28,14 @@ export const FETCH_USED_ITEM = gql`
 
 export const useQueryFetchUsedItem = (): typeof result => {
   const router = useRouter();
-  const result = useQuery<Pick<IQuery, "fetchUseditem">, IQueryFetchBoardArgs>(
-    FETCH_USED_ITEM,
-    {
-      variables: {
-        useditemId: router.query.page,
-      },
-    }
-  );
+
+  const result = useQuery<
+    Pick<IQuery, "fetchUseditem">,
+    IQueryFetchUseditemArgs
+  >(FETCH_USED_ITEM, {
+    variables: {
+      useditemId: String(router.query.page),
+    },
+  });
   return result;
 };
