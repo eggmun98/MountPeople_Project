3c5175ee6b0fe940d2d10a105d8d3d77d1c474c0
eggmun98 <eggmun98@gmail.com>
2023-05-03 16:02:31 +0900

    [2023.05.03] Docs 배포 준비

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   webpack5: true,
   webpack: (config, options) => {
     config.cache = false;
