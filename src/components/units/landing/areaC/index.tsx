195ca1317d9c36eb03abfb0029eb82e943e01367
eggmun98 <eggmun98@gmail.com>
2023-05-07 18:58:48 +0900

    [2023.05.07] Docs 랜딩페이지 스타일 수정, 기능 추가 및 수정

--- src/components/units/landing/areaC/index.tsx
+++ src/components/units/landing/areaC/index.tsx
@@ -1,11 +1,16 @@
 import * as A from "../styles";
+import { IProps } from "./types";
 
-export default function AreaC(): JSX.Element {
+export default function AreaC(props: IProps): JSX.Element {
   return (
-    <A.Container>
+    <A.Container ref={props.scrollRefC}>
       <video src="/landing/area03.mp4" muted autoPlay loop></video>
-
-      <A.ScrollBox>
+      <div>
+        <h1>안쓰는 등산물품?</h1>
+        <h2>Mount People</h2>
+        <h3>로 팔아보자~</h3>
+      </div>
+      <A.ScrollBox onClick={props.onClickArea(props.scrollRefD)}>
         <A.ScrollText>SCROLL</A.ScrollText>
         <A.ScrollIcon src="/landing/arrow01.png" />
       </A.ScrollBox>
--- src/components/units/landing/index.tsx
+++ src/components/units/landing/index.tsx
@@ -1,19 +1,32 @@
+import { useRef } from "react";
 import AreaA from "./areaA";
 import AreaB from "./areaB";
 import AreaC from "./areaC";
 import AreaD from "./areaD";
 import Header from "./header";
 import { Wrapper } from "./styles";
+import { useAreaScrollMode } from "../../commons/hooks/customs/useAreaScrollMode";
 
 export default function Lending(): JSX.Element {
+  const { onClickArea, scrollRefB, scrollRefC, scrollRefD } =
+    useAreaScrollMode();
+
   return (
     <div style={{ position: "relative" }}>
       <Header></Header>
       <Wrapper>
-        <AreaA />
-        <AreaB />
-        <AreaC />
-        <AreaD />
+        <AreaA onClickArea={onClickArea} scrollRefB={scrollRefB} />
+        <AreaB
+          onClickArea={onClickArea}
+          scrollRefB={scrollRefB}
+          scrollRefC={scrollRefC}
+        />
+        <AreaC
+          onClickArea={onClickArea}
+          scrollRefC={scrollRefC}
+          scrollRefD={scrollRefD}
+        />
+        <AreaD scrollRefD={scrollRefD} />
       </Wrapper>
     </div>
   );
